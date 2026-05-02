c4bce1d5eb051fc34e2dc67306d6eea44d05348c
sanghoon1027 <sanghoon135@gmail.com>
2026-05-02 16:27:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// absolutes_len은 배열 absolutes의 길이입니다.
+// signs_len은 배열 signs의 길이입니다.
+int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
+    int answer = 0;
+    for(int i=0;i<absolutes_len;i++){
+        if(signs[i]==1){
+        answer+=(absolutes[i]);
+        }
+        else{
+            answer-=(absolutes[i]);
+        }
+    }
+    return answer;
+}
